5885e3f7b34b6b1efbc53a3d5f087b11f6d812f1
ZiviZ <tal-skala@ziviz.us>
2018-10-17 20:07:18 -0400

    Bump version

--- config/configuration.go
+++ config/configuration.go
@@ -43,7 +43,7 @@ var SessionStore *sessions.CookieStore
 var Configuration ConfigurationSettings
 
 //ApplicationVersion Current version of application. This should be incremented every release
-var ApplicationVersion = "1.0.0.0"
+var ApplicationVersion = "1.0.1.0"
 
 //SessionVariableName is used when checking cookies
 var SessionVariableName = "gib-session"
