df7755c897a86342aa2077ed8eb069dfbdc608b4
ZiviZ <tal-skala@ziviz.us>
2022-02-02 17:00:16 -0500

    Add go mod and fix dependency error

---
+++ go.mod
@@ -0,0 +1,22 @@
+module go-image-board
+
+go 1.17
+
+require (
+	github.com/disintegration/imageorient v0.0.0-20180920195336-8147d86e83ec
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/gorilla/csrf v1.7.1
+	github.com/gorilla/mux v1.8.0
+	github.com/gorilla/securecookie v1.1.1
+	github.com/gorilla/sessions v1.2.1
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
+	github.com/satori/go.uuid v1.2.0
+	golang.org/x/crypto v0.0.0-20220131195533-30dcbda58838
+	golang.org/x/image v0.0.0-20211028202545-6944b10bf410
+)
+
+require (
+	github.com/disintegration/gift v1.1.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
+)
--- plugins/mariadbplugin/AccountTokenFunctions.go
+++ plugins/mariadbplugin/AccountTokenFunctions.go
@@ -21,7 +21,7 @@ func (DBConnection *MariaDBPlugin) ValidateToken(userName string, tokenID string
 	}
 	if err != nil && validTokenID.Valid && validTokenIP.Valid {
 		//User's token in DB is blank
-		logging.WriteLog(logging.LogLevelError,"MariaDBPlugin/ValidateToken", userName, logging.ResultFailure, []string{"Token Invalid", userName, tokenID, ip})
+		logging.WriteLog(logging.LogLevelError, "MariaDBPlugin/ValidateToken", userName, logging.ResultFailure, []string{"Token Invalid", userName, tokenID, ip})
 		return errors.New("Token invalid")
 	}
 
@@ -34,13 +34,13 @@ func (DBConnection *MariaDBPlugin) ValidateToken(userName string, tokenID string
 
 	if validTokenIP.String != ip {
 		//Token is registered for a different IP
-		logging.WriteLog(logging.LogLevelError,"MariaDBPlugin/ValidateToken", userName, logging.ResultFailure, []string{"Token for a different IP", userName, tokenID, ip})
+		logging.WriteLog(logging.LogLevelError, "MariaDBPlugin/ValidateToken", userName, logging.ResultFailure, []string{"Token for a different IP", userName, tokenID, ip})
 		return errors.New("Token invalid")
 	}
 
 	if bytes.Equal(UUIDBytes.Bytes(), uuid.FromStringOrNil(validTokenID.String).Bytes()) == false {
 		//Tokens do not match
-		logging.WriteLog(logging.LogLevelError,"MariaDBPlugin/ValidateToken", userName, logging.ResultFailure, []string{"Tokens don't match", userName, tokenID, ip})
+		logging.WriteLog(logging.LogLevelError, "MariaDBPlugin/ValidateToken", userName, logging.ResultFailure, []string{"Tokens don't match", userName, tokenID, ip})
 		return errors.New("Token invalid")
 	}
 
@@ -49,14 +49,10 @@ func (DBConnection *MariaDBPlugin) ValidateToken(userName string, tokenID string
 
 //GenerateToken Generate a cookie token (string token, or error)
 func (DBConnection *MariaDBPlugin) GenerateToken(userName string, ip string) (string, error) {
-	newToken, err := uuid.NewV4()
+	newToken := uuid.NewV4()
+	_, err := DBConnection.DBHandle.Exec("UPDATE Users SET TokenID=?, IP=? WHERE Name = ?", newToken.String(), ip, userName)
 	if err != nil {
-		logging.WriteLog(logging.LogLevelError,"MariaDBPlugin/GenerateToken", userName, logging.ResultFailure, []string{"Failed to generate a token!", userName, ip, err.Error()})
-		return "", errors.New("failed to generate a token")
-	}
-	_, err = DBConnection.DBHandle.Exec("UPDATE Users SET TokenID=?, IP=? WHERE Name = ?", newToken.String(), ip, userName)
-	if err != nil {
-		logging.WriteLog(logging.LogLevelError,"MariaDBPlugin/GenerateToken", userName, logging.ResultFailure, []string{"Failed to save token", userName, ip, err.Error()})
+		logging.WriteLog(logging.LogLevelError, "MariaDBPlugin/GenerateToken", userName, logging.ResultFailure, []string{"Failed to save token", userName, ip, err.Error()})
 		return "", errors.New("failed to generate a token, check if user exists")
 	}
 	return newToken.String(), nil
@@ -66,9 +62,9 @@ func (DBConnection *MariaDBPlugin) GenerateToken(userName string, ip string) (st
 func (DBConnection *MariaDBPlugin) RevokeToken(userName string) error {
 	_, err := DBConnection.DBHandle.Exec("UPDATE Users SET TokenID=NULL, IP=NULL WHERE Name = ?", userName)
 	if err == nil {
-		logging.WriteLog(logging.LogLevelError,"MariaDBPlugin/RevokeToken", userName, logging.ResultSuccess, []string{"Token revoked!", userName})
+		logging.WriteLog(logging.LogLevelError, "MariaDBPlugin/RevokeToken", userName, logging.ResultSuccess, []string{"Token revoked!", userName})
 	} else {
-		logging.WriteLog(logging.LogLevelError,"MariaDBPlugin/RevokeToken", userName, logging.ResultFailure, []string{"Token not revoked", userName, err.Error()})
+		logging.WriteLog(logging.LogLevelError, "MariaDBPlugin/RevokeToken", userName, logging.ResultFailure, []string{"Token not revoked", userName, err.Error()})
 	}
 	return err
 }
